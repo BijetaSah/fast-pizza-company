c12495fafaa08bd0c8ea46bfd4476473d996f806
BijetaSah <bijetasah@30gmail.com>
2025-11-02 14:11:15 +0530

    create selector function to get user name from userSlice

--- src/features/user/UserName.jsx
+++ src/features/user/UserName.jsx
@@ -1,7 +1,8 @@
 import { useSelector } from "react-redux";
+import { getUserName } from "./userSlice";
 
 function UserName() {
-  const userName = useSelector((state) => state.user.userName);
+  const userName = useSelector(getUserName);
   if (!userName) return;
   return (
     <p className="hidden text-sm font-bold tracking-wider text-stone-800 uppercase sm:block md:text-base">

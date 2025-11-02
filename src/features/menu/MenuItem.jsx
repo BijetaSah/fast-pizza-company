5e68cac8cb4defef3c3fa1976dc30588df075b7f
BijetaSah <bijetasah@30gmail.com>
2025-11-02 14:04:17 +0530

    add  functionality on add to cart button on menu item

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,8 +1,24 @@
 import { formatCurrency } from "../../utilities/helpers";
 import Button from "../../ui/Button";
+import { useDispatch } from "react-redux";
+import { addItem } from "../cart/cartSlice";
 
 function MenuItem({ pizza }) {
+  const dispatch = useDispatch();
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+  function handleAddItemCart() {
+    const quantity = 1;
+    const newItem = {
+      id,
+      name,
+      unitPrice,
+      quantity,
+      totalPrice: unitPrice * quantity,
+    };
+    console.log(newItem);
+    dispatch(addItem(newItem));
+  }
 
   return (
     <li className="flex gap-4 border-b border-stone-200 py-2 last:border-b-0">
@@ -22,7 +38,11 @@ function MenuItem({ pizza }) {
           ) : (
             <p className="text-stone-500">Sold out</p>
           )}
-          <Button type="small">Add to cart</Button>
+          {!soldOut && (
+            <Button onClick={handleAddItemCart} type="small">
+              Add to cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
